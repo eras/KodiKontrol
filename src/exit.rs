4696cebbce89d2427e35158f6ff2b3f11e5aaf11
Erkki Seppälä <erkki.seppala@vincit.fi>
2021-07-29 17:33:43 +0300

    exit: added compatibility with crossbeam
    Now both tokyo- and crossbeam-based functions can use the same exit
    signaling!

--- src/exit.rs
+++ src/exit.rs
@@ -4,13 +4,19 @@ use tokio::sync::watch;
 pub struct Exit {
     level_rx: watch::Receiver<bool>,
     level_tx: Arc<Mutex<watch::Sender<bool>>>,
+    crossbeam_tx: Arc<Mutex<Vec<crossbeam_channel::Sender<()>>>>,
 }
 
 impl Clone for Exit {
     fn clone(&self) -> Self {
         let level_rx = self.level_rx.clone();
         let level_tx = self.level_tx.clone();
-        Exit { level_rx, level_tx }
+        let crossbeam_tx = self.crossbeam_tx.clone();
+        Exit {
+            level_rx,
+            level_tx,
+            crossbeam_tx,
+        }
     }
 }
 
@@ -18,7 +24,12 @@ impl Exit {
     pub fn new() -> Exit {
         let (level_tx, level_rx) = watch::channel(false);
         let level_tx = Arc::new(Mutex::new(level_tx));
-        Exit { level_rx, level_tx }
+        let crossbeam_tx = Arc::new(Mutex::new(vec![]));
+        Exit {
+            level_rx,
+            level_tx,
+            crossbeam_tx,
+        }
     }
 
     pub fn signal(&self) {
@@ -27,6 +38,14 @@ impl Exit {
             .unwrap()
             .send(true)
             .expect("Failed to send exit signal");
+        match self.crossbeam_tx.lock() {
+            Ok(tx) => {
+                for tx in tx.iter() {
+                    let _ignore = tx.send(());
+                }
+            }
+            Err(_) => log::error!("exit::signal failed to lock crossbeam_tx"),
+        }
     }
 
     pub async fn wait(&mut self) {
@@ -36,5 +55,21 @@ impl Exit {
                 .await
                 .expect("Failed to wait for exit level change");
         }
+    }
+
+    pub fn crossbeam_subscribe(&mut self) -> crossbeam_channel::Receiver<()> {
+        let (tx, rx) = crossbeam_channel::unbounded();
+        self.crossbeam_tx.lock().unwrap().push(tx);
+        if *self.level_rx.borrow() {
+            match self.crossbeam_tx.lock() {
+                Ok(tx) => {
+                    for tx in tx.iter() {
+                        let _ignore = tx.send(());
+                    }
+                }
+                Err(_) => log::error!("exit::signal failed to lock crossbeam_tx"),
+            }
+        }
+        rx
     }
 }
